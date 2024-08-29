656990cdc703c9f083af0af3b28e1ef08c965c9b
Sebastian Ballen <sebastian.ballenc@gmail.com>
2024-08-29 17:36:16 -0500

    feat: new file created, components data types.

---
+++ src/types/components.ts
@@ -0,0 +1,23 @@
+interface MainAttachment {
+  big: string;
+  small: string;
+}
+
+interface Link {
+  rel: string;
+  uri: string;
+  methods: string;
+}
+
+export interface Image {
+  type?: string;
+  id?: number;
+  title: string;
+  price: number
+  author: string;
+  created_at?: string;
+  main_attachment: MainAttachment;
+  likes_count?: number;
+  liked?: boolean;
+  links?: Link[];
+}
