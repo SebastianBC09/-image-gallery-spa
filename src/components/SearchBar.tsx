73cb9c4473a402b8a1d5090444aeff181971db7b
Sebastian Ballen <sebastian.ballenc@gmail.com>
2024-08-30 23:31:31 -0500

    feat: handle inputs & handle submit added to searchbar

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -3,14 +3,24 @@ import logo from '../assets/logo.svg';
 import { Searchbar } from '../types/components.ts'
 
 const SearchBar: React.FC<Searchbar> = ({ onSearch }) => {
+  const [query, setQuery] = React.useState('');
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setQuery(e.target.value);
+  };
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    onSearch(query);
+  };
   return (
     <header className="w-auto h-[180px] bg-white flex flex-row justify-between items-center mb-[40px] px-[12.4%]">
       <div>
         <img src={logo} alt="logo" />
       </div>
       <search>
-        <form>
-          <input placeholder="You're looking for something?" />
+        <form onSubmit={handleSubmit}>
+          <input placeholder="You're looking for something?" value={query} onChange={handleInputChange} />
         </form>
       </search>
     </header>
