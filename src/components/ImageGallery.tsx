5781a1c95cc3a6e014f94e6609c611255d0ad584
Sebastian Ballen <sebastian.ballenc@gmail.com>
2024-08-30 23:32:03 -0500

    fix: key index error fixed

--- src/components/ImageGallery.tsx
+++ src/components/ImageGallery.tsx
@@ -2,13 +2,13 @@ import React from 'react';
 import useImageGallery from '../hooks/useImageGallery';
 import { ImageGallery } from '../types/components.ts';
 
-const ImagesGallery: React.FC<ImageGallery> = ({ children }) => {
-  const { displayedImages, loading, lastImageElementRef } = useImageGallery();
+const ImagesGallery: React.FC<ImageGallery> = ({ children, searchQuery }) => {
+  const { displayedImages, loading, lastImageElementRef } = useImageGallery(searchQuery);
 
   return (
     <div className="flex flex-row flex-wrap justify-center gap-[20px]">
       {displayedImages.map((image, index) => (
-        <div key={image.id} ref={index === displayedImages.length - 1 ? lastImageElementRef : null}>
+        <div key={index} ref={index === displayedImages.length - 1 ? lastImageElementRef : null}>
           {children(image, index === displayedImages.length - 1 ? lastImageElementRef : null)}
         </div>
       ))}
